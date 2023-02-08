d70297daad09d0b85d4bab8add5f5a0528629fd2
mo.do.ru.gg <mo.do.ru.gg@gmail.com>
2023-02-08 14:04:20 +0800

    фикс экспертизы мээ в контроле зпз

--- KmsReportWS/Collector/ConsolidateReport/ControlZpzCollector.cs
+++ KmsReportWS/Collector/ConsolidateReport/ControlZpzCollector.cs
@@ -11,7 +11,7 @@ namespace KmsReportWS.Collector.ConsolidateReport
         private readonly string[] _themes = { "Таблица 5", "Таблица 6", "Таблица 8", "Таблица 10", "Таблица 11" };
 
         private readonly string[] _rowNumsExpertiseTable5 = { "4.1", "4.2", "4.3", "4.4", "4.5", "4.6" };
-        private readonly string[] _rowNumsExpertiseTable6 = { "5", };
+        private readonly string[] _rowNumsExpertiseTable6 = { "5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.7" };
         private readonly string[] _rowNumsExpertiseTable8 =
             {"6.1", "6.2", "6.3", "6.4", "6.5", "6.6", "6.7", "6.8", "6.9", "6.10"};
 
@@ -137,6 +137,8 @@ namespace KmsReportWS.Collector.ConsolidateReport
                 CaseMeePlan = table6Data.Where(x => x.RowNum == "2").Sum(x => x.SumVidpomAnother),
                 DefectMeeTarget = table6Data.Where(x => _rowNumsExpertiseTable6.Contains(x.RowNum)).Sum(x => x.SumVidpom),
                 DefectMeePlan = table6Data.Where(x => _rowNumsExpertiseTable6.Contains(x.RowNum)).Sum(x => x.SumVidpomAnother),
+                //DefectMeeTarget = table6Data.Where(x => x.RowNum == "6").Sum(x => x.SumVidpom),
+                //DefectMeePlan = table6Data.Where(x => x.RowNum == "6").Sum(x => x.SumVidpomAnother),
                 EkmpTarget = table8Data.Where(x => x.RowNum == "1").Sum(x => x.SumVidpom),
                 EkmpPlan = table8Data.Where(x => x.RowNum == "1").Sum(x => x.SumVidpomAnother),
                 ThemeCaseEkmpPlan = table8Data.Where(x => x.RowNum == "3").Sum(x => x.SumVidpomAnother),
