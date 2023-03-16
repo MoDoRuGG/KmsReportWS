6670694e123beb2dde116815f17d3a9d90c46f0a
mo.do.ru.gg <mo.do.ru.gg@gmail.com>
2023-03-16 11:51:12 +0800

    fix 1.1.7.36

--- KmsReportWS/Service/EmailSender.cs
+++ KmsReportWS/Service/EmailSender.cs
@@ -12,7 +12,7 @@ namespace KmsReportWS.Service
         private const string NameSender = "KmsReport";
         private const string User = "admin.kmsreport";
         private const string Password = "jwJdWagc4BT3";
-        private const string SmtpServer = "mail.kapmed.ru";
+        private const string SmtpServer = "smtp.kapmed.ru";
         private const string Sign = "\r\nДанное письмо сформировано автоматически. Пожалуйста, не отвечайте на него.";
         private const int SmtpPort = 587;
 
