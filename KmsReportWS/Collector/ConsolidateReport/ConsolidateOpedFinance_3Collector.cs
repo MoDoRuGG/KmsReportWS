f91ab7dfd40d1114699f975fe73f84feb2b9ea23
mo.do.ru.gg <mo.do.ru.gg@gmail.com>
2023-06-15 14:54:09 +0800

    фиксация

---
+++ KmsReportWS/Collector/ConsolidateReport/ConsolidateOpedFinance_3Collector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using KmsReportWS.LinqToSql;
+using KmsReportWS.Model.ConcolidateReport;
+using KmsReportWS.Properties;
+using KmsReportWS.Support;
+
+namespace KmsReportWS.Collector.ConsolidateReport
+{
+    public class ConsolidateOpedFinance_3Collector
+    {
+        public List<ConsolidateOpedFinance_3> Collect(string year)
+        {
+            List<ConsolidateOpedFinance_3> result = new List<ConsolidateOpedFinance_3>();
+            try
+            {
+                using (MsConnection connect = new MsConnection(Settings.Default.ConnStr))
+                {
+                    connect.NewSp("p_ConsolidateOpedFinance_3");
+                    connect.AddSpParam("@year", year);
+                    var dt = connect.DataTable();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        result.Add(new ConsolidateOpedFinance_3
+                        {
+                            RegionName = row["RegionName"].ToString(),
+                            IdRegion = row["IdRegion"].ToString(),
+                            Yymm = row["Yymm"].ToString(),
+                            Fact = row.ToDecimal("Fact"),
+                            PlanO = row.ToDecimal("PlanO"),
+                            Notes = row["Notes"].ToString()
+                        });
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ KmsReportWS/Model/ConcolidateReport/ConsolidateOpedFinance_3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KmsReportWS.Model.ConcolidateReport
+{
+    public class ConsolidateOpedFinance_3
+    {
+        public string  RegionName { get; set; }
+        public string  IdRegion { get; set; }
+        public string   Yymm { get; set; }
+        public decimal? Fact { get; set; }
+        public decimal? PlanO { get; set; }
+        public decimal? Percent { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
